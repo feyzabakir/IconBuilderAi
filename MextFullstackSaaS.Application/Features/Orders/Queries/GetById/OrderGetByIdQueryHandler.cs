6a39886971edd9a129eb49765ac70139f2f1cef2
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-06-17 14:33:20 +0300

    Add project files.

---
+++ MextFullstackSaaS.Application/Features/Orders/Queries/GetById/OrderGetByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using MextFullstackSaaS.Application.Common.Helpers;
+using MextFullstackSaaS.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MextFullstackSaaS.Application.Features.Orders.Queries.GetById
+{
+    public class OrderGetByIdQueryHandler : IRequestHandler<OrderGetByIdQuery, OrderGetByIdDto>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMemoryCache _memoryCache;
+
+        public OrderGetByIdQueryHandler(IApplicationDbContext dbContext, IMemoryCache memoryCache)
+        {
+            _dbContext = dbContext;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task<OrderGetByIdDto> Handle(OrderGetByIdQuery request, CancellationToken cancellationToken)
+        {
+            OrderGetByIdDto order;
+
+            if (_memoryCache.TryGetValue(MemoryCacheHelper.GetOrderGetByIdKey(request.Id), out order))
+                return order;
+
+            order = await _dbContext
+                .Orders
+                .AsNoTracking()
+                .Select(x => OrderGetByIdDto.MapFromOrder(x))
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            _memoryCache.Set(MemoryCacheHelper.GetOrderGetByIdKey(request.Id), order, MemoryCacheHelper.GetMemoryCacheEntryOptions());
+
+            return order;
+        }
+    }
+}
---
+++ MextFullstackSaaS.WasmClient/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.{razor,html,cshtml}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans-serif'],
+                // You can add other font families if needed
+            },
+        },
+    },
+    plugins: [
+        require('daisyui'),
+    ],
+    daisyui: {
+        themes: [
+            "dark",
+            "black",
+            "night",
+            "sunset"
+        ],
+    },
+}
